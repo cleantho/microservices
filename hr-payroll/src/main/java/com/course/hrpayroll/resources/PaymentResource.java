15cda1524914a5d056743062fcc0205bccea177e
cleantho <cleantho@gmail.com>
2023-01-27 20:50:09 -0300

    Configuring Eureka clients

--- hr-payroll/src/main/java/com/course/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/course/hrpayroll/resources/PaymentResource.java
@@ -17,7 +17,6 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service; 
 	
-	
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment obj = service.getPayment(workerId, days);
