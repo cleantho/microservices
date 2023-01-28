3bdd49ee08fc7e3489c21f8bf2fd384c93f81c56
cleantho <cleantho@gmail.com>
2023-01-28 19:06:59 -0300

    Resilience4J fault tolerance

--- hr-payroll/src/main/java/com/course/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/course/hrpayroll/resources/PaymentResource.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.course.hrpayroll.entities.Payment;
 import com.course.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
@@ -17,9 +19,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service; 
 	
+	@CircuitBreaker(name = "getPayment", fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment obj = service.getPayment(workerId, days);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days, Throwable e){
+		Payment obj = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok().body(obj);
 	}
 }
