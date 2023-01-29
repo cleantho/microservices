0a8e00ba705ee13847923a51c323aa5bb0209485
cleantho <cleantho@gmail.com>
2023-01-28 22:45:58 -0300

    Configuring api gateway

---
+++ hr-api-gateway/src/main/java/com/course/hrapigateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.course.hrapigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+	@Bean
+	RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/payments/**").uri("lb://HR-PAYROLL"))
+				.route(r -> r.path("/workers/**").uri("lb://HR-WORKER"))
+				.route(r -> r.path("/workers").uri("lb://HR-WORKER"))
+				.build();
+	}
+
+}
